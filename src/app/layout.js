86633fac0d956f0c3424c9b1e49fa1449218a574
lucky <aakashrecb@gmail.com>
2025-11-05 13:40:09 +0530

    script tag updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+
     <html lang="en">
+    <head>
+       <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VPMXVFHM8T"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VPMXVFHM8T');
+          `,
+          }}
+        />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${prata.variable} antialiased`}
       >
